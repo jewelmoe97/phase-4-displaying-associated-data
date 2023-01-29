2ca4aa3c29c681f654bf49817acbee21d8c684df
jewel <snowdecember079@gmail.com>
2023-01-29 13:09:51 -0500

    pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+    def index
+        reviews = Review.all.order(rating: :desc)
+        render json: reviews, include: :dog_house
+      end
+    
 end
